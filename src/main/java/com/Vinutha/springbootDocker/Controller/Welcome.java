393765d168cebb686de57c856fc077ac7df13d81
vinutha <vinutha418@gmail.com>
2022-12-09 12:27:41 -0500

    first commit

---
+++ src/main/java/com/Vinutha/springbootDocker/Controller/Welcome.java
@@ -0,0 +1,23 @@
+package com.Vinutha.springbootDocker.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Welcome {
+    @GetMapping("/")
+    public String welcome(){
+        return "welcome";
+    }
+
+
+   /* echo "# SpringBootDeploy" >> README.md
+    git init
+    git add README.md
+    git commit -m "first commit"
+    git branch -M main
+    git remote add origin https://github.com/Viinutha/SpringBootDeploy.git
+    git push -u origin main*/
+}
+
