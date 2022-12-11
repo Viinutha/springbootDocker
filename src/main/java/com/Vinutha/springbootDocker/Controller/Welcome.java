559a6166c664080e6070d285e36f362f5a6a7e33
vinutha <vinutha418@gmail.com>
2022-12-10 23:29:03 -0500

    try

--- src/main/java/com/Vinutha/springbootDocker/Controller/Welcome.java
+++ src/main/java/com/Vinutha/springbootDocker/Controller/Welcome.java
@@ -18,6 +18,7 @@ public class Welcome {
     git commit -m "first commit"
     git branch -M main
     git remote add origin https://github.com/Viinutha/SpringBootDeploy.git
-    git push -u origin main*/
+    git push -u origin main
+    git update-index --chmod=+x gradlew */
 }
 
